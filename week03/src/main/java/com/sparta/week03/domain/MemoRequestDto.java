c5447c833d217782c04abdc689e3cc2265a8f7d5
kmj <mjmjmj9840@gmail.com>
2021-07-11 18:18:23 +0900

    [3주차] Memo Service 생성

--- week03/src/main/java/com/sparta/week03/domain/MemoRequestDto.java
+++ week03/src/main/java/com/sparta/week03/domain/MemoRequestDto.java
@@ -3,8 +3,7 @@ package com.sparta.week03.domain;
 import lombok.Getter;
 
 @Getter
-public class MemoReqeustDto {
+public class MemoRequestDto {
     private String username;
     private String contents;
 }
-
