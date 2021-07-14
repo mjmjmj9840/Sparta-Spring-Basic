ceb17b847292aaf1d53b709cd8758af71d1b4ef1
kmj <mjmjmj9840@gmail.com>
2021-07-14 18:56:19 +0900

    [4주차] 관심 상품 조회 기능 추가

---
+++ week04/src/main/java/com/sparta/week04/models/Product.java
@@ -0,0 +1,32 @@
+package com.sparta.week04.models;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Product extends Timestamped {
+
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Id
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String image;
+
+    @Column(nullable = false)
+    private String link;
+
+    @Column(nullable = false)
+    private int lprice;
+
+    @Column(nullable = false)
+    private int myprice;
+}
