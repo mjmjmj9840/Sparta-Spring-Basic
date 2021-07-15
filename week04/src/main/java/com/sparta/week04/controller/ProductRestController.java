98df1b177005cb25ab8f3231bc8fc45d285598b8
kmj <mjmjmj9840@gmail.com>
2021-07-15 19:41:44 +0900

    [4주차] 최저가 변경 기능 추가

--- week04/src/main/java/com/sparta/week04/controller/ProductRestController.java
+++ week04/src/main/java/com/sparta/week04/controller/ProductRestController.java
@@ -1,13 +1,12 @@
 package com.sparta.week04.controller;
 
 import com.sparta.week04.models.Product;
+import com.sparta.week04.models.ProductMypriceRequestDto;
 import com.sparta.week04.models.ProductRepository;
 import com.sparta.week04.models.ProductRequestDto;
+import com.sparta.week04.service.ProductService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -15,6 +14,7 @@ import java.util.List;
 @RestController
 public class ProductRestController {
 
+    private final ProductService productService;
     private final ProductRepository productRepository;
 
     @GetMapping("/api/products")
@@ -26,5 +26,10 @@ public class ProductRestController {
     public Product createProduct(@RequestBody ProductRequestDto requestDto) {
         Product product = new Product(requestDto);
         return productRepository.save(product);
+    }
+
+    @PutMapping("/api/products/{id}")
+    public Long updateProduct(@PathVariable Long id, @RequestBody ProductMypriceRequestDto requestDto) {
+        return productService.update(id, requestDto);
     }
 }
