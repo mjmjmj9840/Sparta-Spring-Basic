3c178c89cbc694ed088a55a571c1a3bcd7595c11
kmj <mjmjmj9840@gmail.com>
2021-07-15 16:58:57 +0900

    [4주차] 키워드로 상품 검색 기능 추가

--- week04/src/main/java/com/sparta/week04/utils/NaverShopSearch.java
+++ week04/src/main/java/com/sparta/week04/utils/NaverShopSearch.java
@@ -1,11 +1,19 @@
 package com.sparta.week04.utils;
 
+import com.sparta.week04.models.ItemDto;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.http.*;
+import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
 public class NaverShopSearch {
 
-    public String search() {
+    public String search(String query) {
         RestTemplate rest = new RestTemplate();
         HttpHeaders headers = new HttpHeaders();
         headers.add("X-Naver-Client-Id", "clientId");
@@ -13,7 +21,7 @@ public class NaverShopSearch {
         String body = "";
 
         HttpEntity<String> requestEntity = new HttpEntity<String>(body, headers);
-        ResponseEntity<String> responseEntity = rest.exchange("https://openapi.naver.com/v1/search/shop.json?query=아디다스", HttpMethod.GET, requestEntity, String.class);
+        ResponseEntity<String> responseEntity = rest.exchange("https://openapi.naver.com/v1/search/shop.json?query=" + query, HttpMethod.GET, requestEntity, String.class);
         HttpStatus httpStatus = responseEntity.getStatusCode();
         int status = httpStatus.value();
         String response = responseEntity.getBody();
@@ -23,8 +31,18 @@ public class NaverShopSearch {
         return response;
     }
 
-    public static void main(String[] args) {
-        NaverShopSearch naverShopSearch = new NaverShopSearch();
-        naverShopSearch.search();
+    public List<ItemDto> fromJSONtoItems(String result) {
+        JSONObject rjson = new JSONObject(result);
+        JSONArray items = rjson.getJSONArray("items");
+
+        List<ItemDto> itemDtoList = new ArrayList<>();
+
+        for (int i = 0; i < items.length(); i++) {
+            JSONObject itemJson = items.getJSONObject(i);
+            ItemDto itemDto = new ItemDto(itemJson);
+            itemDtoList.add(itemDto);
+        }
+
+        return itemDtoList;
     }
 }
